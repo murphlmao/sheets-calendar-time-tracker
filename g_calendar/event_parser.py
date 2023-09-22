557e07c95023b1fad0edb2d94c5e2a1a07e426aa
murphlmao <murphyjmalcolm@gmail.com>
2023-09-21 20:56:24 -0400

    feat: recent 10 events parser

---
+++ g_calendar/event_parser.py
@@ -0,0 +1,20 @@
+import datetime
+
+from g_calendar import service
+
+def get_ten_events():
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                            maxResults=10, singleEvents=True,
+                                            orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+
+    # Prints the start and name of the next 10 events
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from g_calendar import event_parser
 
 def main():
+    event_parser.get_ten_events()
     pass
 
 if __name__ == "__main__":
